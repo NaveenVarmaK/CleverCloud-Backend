91c26f3243033738bbaea97d0275043edfc6a035
Naveen Varma Kalidindi <64104378+NaveenVarmaK@users.noreply.github.com>
2024-01-10 16:38:48 +0100

    Update UserController.java

--- src/main/java/com/example/parking/controller/UserController.java
+++ src/main/java/com/example/parking/controller/UserController.java
@@ -35,10 +35,10 @@ public class UserController {
     @ModelAttribute
     public void setVaryResponseHeader(HttpServletResponse response) {
         // remove vary header if present
-        response.setHeader("Vary", null);
+        response.setHeader("Vary", "");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 
     @GetMapping("/users")
